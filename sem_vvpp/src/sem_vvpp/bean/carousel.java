4231397d20a15090d9e97eb69c2781e78d73fd91
free6ear <free6ear@gmail.com>
2019-07-24 01:27:47 +0800

    t

--- sem_vvpp/src/sem_vvpp/bean/carousel.java
+++ sem_vvpp/src/sem_vvpp/bean/carousel.java
@@ -1,5 +1,28 @@
 package sem_vvpp.bean;
 
-public class carousel {
+import java.util.Date;
 
+public class carousel {
+	private int id;
+	private String title;
+	private String paperFile;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPaperFile() {
+		return paperFile;
+	}
+	public void setPaperFile(String paperFile) {
+		this.paperFile = paperFile;
+	}	
 }
