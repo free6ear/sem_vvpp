7aeaba4fa83060defdcd8b2ed4655e8cd7c9461c
free6ear <free6ear@gmail.com>
2019-07-29 01:07:22 +0800

    7/29 dawn

--- sem_vvpp/src/servlet/addPaperServlet.java
+++ sem_vvpp/src/servlet/addPaperServlet.java
@@ -1,5 +1,7 @@
 package servlet;
 
+import bean.PaperInfo;
+import dao.PaperInfoDAO;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
@@ -10,70 +12,120 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class addPaperServlet extends HttpServlet {
 
+    ArrayList formInfo = new ArrayList();
+    String title = null;
+    String author = null;
+    String path = null;
+    
+    PaperInfo bean = new PaperInfo();
+    PaperInfoDAO dao = new PaperInfoDAO();
+    
+    //上传文件存储路径
+    protected static final String UPLOAD_DIRECTORY = "papers_upload";
+
+    //上传配置
+    private static final int MEMORY_THRESHOLD = 1024 * 1024 * 3;
+    private static final int MAX_FILE_SIZE = 1024 * 1024 * 40;
+    private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 50;
+
+    /*
+    *上传数据及保存文件
+    */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
 
-        String filename = null;
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PaperInfo paper_info = new PaperInfo();
+        PaperInfoDAO pdao = new PaperInfoDAO();
+
+
+        //检测是否为多媒体上传
+        if (!ServletFileUpload.isMultipartContent(request)) {
+             //如果不是则停止
+            PrintWriter writer = response.getWriter();
+            writer.println("Error: 表单必须包含enctype=multipart/form-data");
+            writer.flush();
+            return;
+        }
+
+        //配置上传参数
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+
+        //设置上传参数
+        factory.setSizeThreshold(MEMORY_THRESHOLD);
+
+        //设置内存临界值——超过后将产生临时文件并存储于临时目录中
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+
+        //设置临时存储目录
+        ServletFileUpload upload = new ServletFileUpload(factory);
+
+        //设置最大文件上传值
+        upload.setFileSizeMax(MAX_FILE_SIZE);
+
+        //设置最大请求值（包含文件和表单数据）
+        upload.setFileSizeMax(MAX_REQUEST_SIZE);
+
+        //中文处理
+        upload.setHeaderEncoding("utf-8");
+
+        //构造临时路径来存储上传的文件
+        //该路径相对当前应用的目录
+        String uploadPath = request.getServletContext().getRealPath("./") + File.separator + UPLOAD_DIRECTORY;
+
+        //如果目录不存在则创建
+        File uploadDir = new File(uploadPath);
+        if(!uploadDir.exists()){
+            uploadDir.mkdir();
+        }
 
         try {
-            DiskFileItemFactory factory = new DiskFileItemFactory();
-            ServletFileUpload upload = new ServletFileUpload(factory);
-            //设置上传文件的大小限制为10M
-            factory.setSizeThreshold(10240*10240);
+            //解析请求的内容提取文件数据
+            @SuppressWarnings("unchecked")
+            List<FileItem> formItems = upload.parseRequest(request);
 
-            List items = null;
-            try {
-                items = upload.parseRequest(request);
-            } catch (FileUploadException e) {
+            if (formItems != null && formItems.size() > 0) {
 
-                e.printStackTrace();
-            }
+                //迭代表单数据
+                for (FileItem item : formItems) {
 
-            Iterator iter = items.iterator();
-            while (iter.hasNext()) {
-                FileItem item = (FileItem) iter.next();
-                if (!item.isFormField()) {
+                    if (item.isFormField()) {
 
-                    filename = System.currentTimeMillis() + ".pdf";
-
-                    String paperFolder =request.getServletContext().getRealPath("uploaded");
-
-                    File f = new File(paperFolder, filename);
-                    f.getParentFile().mkdirs();
-
-                    InputStream is = item.getInputStream();
-
-                    FileOutputStream fos = new FileOutputStream(f);
-                    byte b[] = new byte[10240*10240];
-                    int length = 0;
-                    while (-1 != (length = is.read(b))) {
-                        fos.write(b, 0, length);
+                        String formFieldInfo = item.getString();
+                        formFieldInfo = new String(formFieldInfo.getBytes("ISO-8859-1"), "UTF-8");
+                        formInfo.add(formFieldInfo);
                     }
-                    fos.close();
-                } else {
-                    System.out.println(item.getFieldName());
-                    String value = item.getString();
-                    value = new String(value.getBytes("ISO-8859-1"), "UTF-8");
-                    System.out.println(value);
+
+                    //处理不在表单中的字段
+                    if (!item.isFormField()) {
+                        String fileName = new File(item.getName()).getName();
+                        String path = uploadPath + File.separator + fileName;
+                        File StoreFile = new File(path);
+
+                        bean.setPath(path);
+                        
+                        item.write(StoreFile);
+                        response.getWriter().write("<script>alert(\"上传成功!\");</script>");
+                    }
                 }
             }
-            String html = "<img width='200' height='150' src='uploaded/%s' />";
-            response.setContentType("text/html");
-            PrintWriter pw= response.getWriter();
 
-            pw.format(html, filename);
+            bean.setTitle(formInfo.get(0).toString());
+            bean.setAuthor(formInfo.get(1).toString());
 
-        } catch (FileNotFoundException e) {
+            dao.add(bean);
 
-            e.printStackTrace();
-        } catch (Exception e) {
-
-            e.printStackTrace();
+        } catch (Exception ex) {
+            request.setAttribute("message", "错误信息：" + ex.getMessage());
         }
+
+        request.getServletContext().getRequestDispatcher("/message.jsp").forward(request, response);
     }
 }
